aeac3ae9686ee4e788e3f2e4ce617658fc988fe8
Jack Edwards <jackedwards2026@u.northwestern.edu>
2023-07-12 13:48:10 -0400

    rolled back cloudflare changes

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-import adapter from '@sveltejs/adapter-cloudflare';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-  kit: {
-    adapter: adapter({
-        // See below for an explanation of these options
-        routes: {
-            include: ['/*'],
-            exclude: ['<all>']
-        }
-    })
-}
+  plugins: [svelte()]
 })
