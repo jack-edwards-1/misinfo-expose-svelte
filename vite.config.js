ccc808980bce49953b8a5881703aec7b354cddaa
Jack Edwards <jackedwards2026@u.northwestern.edu>
2023-07-12 12:27:52 -0400

    removed redundancies

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import adapter from '@sveltejs/adapter-cloudflare';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte()],
+  kit: {
+    adapter: adapter({
+        // See below for an explanation of these options
+        routes: {
+            include: ['/*'],
+            exclude: ['<all>']
+        }
+    })
+}
 })
